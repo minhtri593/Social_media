9d585971819f6878d20f4ca6e693a3bc9909985c
trilaanh2k3 <trinhminhtri0509@gmail.com>
2024-11-11 20:45:52 +0700

    init code

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php  
+    define("DB_NAME", 'social_media');
+    define("DB_USER", 'social_media');
+    define("DB_PASSWORD", '123456');
+    define("DB_HOST", 'localhost');
+    define("DB_PORT", '3306');
+    define("SYS_URL", 'http://localhost');
+    define("DEBUGGING", false);
+    define("DEFAULT_LOCALE", 'en_es');
+    define("LICENCE_KEY", 'nullcave');
+    define("JWT_SECRET", 'bfac86b30da28ca0434916839dd1f252');
+    ?>
